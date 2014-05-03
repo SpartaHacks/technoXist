3d61f4f2f34336b234c76f17aeb84f179f41b986
Suyash Bhatt <suyash1629@gmail.com>
2014-05-04 02:53:30 +0545

    improved entry display and loading state

--- src/com/technoxist/fragment/EntryFragment.java
+++ src/com/technoxist/fragment/EntryFragment.java
@@ -442,7 +442,7 @@ public class EntryFragment extends SwipeRefreshFragment implements BaseActivity.
             activity.invalidateOptionsMenu();
 
             // Listen the mobilizing task
-            if (FetcherService.hasTasks(mEntriesIds[mCurrentPagerPos])) {
+            if (FetcherService.hasMobilizationTask(mEntriesIds[mCurrentPagerPos])) {
                 showSwipeProgress();
             } else {
                 hideSwipeProgress();
